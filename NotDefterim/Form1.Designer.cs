49f3c14dd16f61f28c43c299659cd3ce8300bb11
qq <qq@DESKTOP-HA541B1>
2025-11-17 23:35:48 +0300

    Proje dosyası ekle.

---
+++ NotDefterim/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace NotDefterim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIcerik = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menYeni = new System.Windows.Forms.ToolStripMenuItem();
+            this.menAc = new System.Windows.Forms.ToolStripMenuItem();
+            this.menKaydet = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menCikis = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtIcerik
+            // 
+            this.txtIcerik.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtIcerik.Location = new System.Drawing.Point(0, 28);
+            this.txtIcerik.Multiline = true;
+            this.txtIcerik.Name = "txtIcerik";
+            this.txtIcerik.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtIcerik.Size = new System.Drawing.Size(800, 422);
+            this.txtIcerik.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menYeni,
+            this.menAc,
+            this.menKaydet,
+            this.toolStripMenuItem2,
+            this.menCikis});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // menYeni
+            // 
+            this.menYeni.Name = "menYeni";
+            this.menYeni.Size = new System.Drawing.Size(224, 26);
+            this.menYeni.Text = "Yeni";
+            this.menYeni.Click += new System.EventHandler(this.menYeni_Click);
+            // 
+            // menAc
+            // 
+            this.menAc.Name = "menAc";
+            this.menAc.Size = new System.Drawing.Size(224, 26);
+            this.menAc.Text = "Aç";
+            this.menAc.Click += new System.EventHandler(this.menAc_Click);
+            // 
+            // menKaydet
+            // 
+            this.menKaydet.Name = "menKaydet";
+            this.menKaydet.Size = new System.Drawing.Size(224, 26);
+            this.menKaydet.Text = "Kaydet";
+            this.menKaydet.Click += new System.EventHandler(this.menKaydet_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(224, 26);
+            this.toolStripMenuItem2.Text = " ";
+            // 
+            // menCikis
+            // 
+            this.menCikis.Name = "menCikis";
+            this.menCikis.Size = new System.Drawing.Size(224, 26);
+            this.menCikis.Text = "Çıkış";
+            this.menCikis.Click += new System.EventHandler(this.menCikis_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtIcerik);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIcerik;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menYeni;
+        private System.Windows.Forms.ToolStripMenuItem menAc;
+        private System.Windows.Forms.ToolStripMenuItem menKaydet;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem menCikis;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ NotDefterim/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotDefterim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menCikis_Click(object sender, EventArgs e)
+         
+        {
+            Application.Exit();
+
+        }
+
+        private void menYeni_Click(object sender, EventArgs e)
+        {
+            txtIcerik.Text = string.Empty;
+        }
+
+        
+            private void menKaydet_Click(object sender, EventArgs e)
+        {
+            // SaveFileDialog'un başlangıç ayarları
+            saveFileDialog1.Filter = "Metin Dosyası (*.txt)|*.txt|Tüm Dosyalar (*.*)|*.*";
+            saveFileDialog1.Title = "Metin Belgesini Kaydet";
+
+            // Kullanıcı Kaydet butonuna bastıysa
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Dosyaya içeriği yazar
+                    System.IO.File.WriteAllText(saveFileDialog1.FileName, txtIcerik.Text);
+                    MessageBox.Show("Dosya başarıyla kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Kaydetme hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void menAc_Click(object sender, EventArgs e)
+        {
+            // OpenFileDialog'un başlangıç ayarları
+            openFileDialog1.Filter = "Metin Dosyası (*.txt)|*.txt|Tüm Dosyalar (*.*)|*.*";
+            openFileDialog1.Title = "Metin Belgesi Aç";
+
+            // Kullanıcı Aç butonuna bastıysa
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Dosyanın içeriğini okur ve metin kutusuna atar
+                    txtIcerik.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Açma hatası: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+    }
+
